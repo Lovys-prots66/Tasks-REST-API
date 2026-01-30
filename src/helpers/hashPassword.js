85590c66662a52f04bf4f1c9af121820e94c0883
Lovys-prots66 <lovys6027@gmail.com>
2026-01-30 13:04:42 +0100

    add password hashing function

---
+++ src/helpers/hashPassword.js
@@ -0,0 +1,12 @@
+import crypto from "node:crypto"
+
+export default function hashPsw(psw){
+    return new Promise((resolve, reject) => {
+        const salt = crypto.randomBytes(16).toString("hex");
+
+        crypto.scrypt(psw, salt, 64, {N: 16384, maxmem : 64 * 1024 * 1024}, (err, derivedKey) => {
+            if(err) reject(err);
+            resolve(['scrypt', salt, derivedKey.toString("hex")].join(":"));
+        })
+    })
+}
